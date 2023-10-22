1cd862bb8eb17180b794995408beba07041ac6ca
jo-project <jo.project.0911@gmail.com>
2023-10-23 09:43:08 +1100

    bar

--- plugin/init.lua
+++ plugin/init.lua
@@ -415,7 +415,7 @@ wezterm.on("update-status", function(window, pane)
       { Attribute = { Intensity = "Bold" } },
       { Background = { Color = "#eba0ac" } },
       { Foreground = { Color = palette.background } },
-      { Text = " "..wezterm.mux.get_active_workspace().." " }
+      { Text = "  "..wezterm.mux.get_active_workspace().." " }
     })
   end
 
